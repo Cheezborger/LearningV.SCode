6c4a7c0b44e0f2bcff7bcd54d42889a989a4ac1a
Cheezborger <115359138+Cheezborger@users.noreply.github.com>
2022-10-08 10:18:39 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var box;
+
+function setup() {
+  createCanvas(1000,1000);
+box=createSprite(200,200,20,20);  
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW))
+  {
+    box.position.x=box.position.x+10;
+  }
+  if(keyIsDown(LEFT_ARROW))
+  {
+    box.position.x=box.position.x-10;
+  }
+  if(keyIsDown(UP_ARROW))
+  {
+    box.position.y=box.position.y-10;
+  }
+  if(keyIsDown(DOWN_ARROW))
+  {
+    box.position.y=box.position.y+10;
+  }
+drawSprites();
+}
+
+
+
+
